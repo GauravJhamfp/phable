cfa699d341341b1b5ffd97c4ad40a6f0e7c49c20
Ronit Singh <ronitsingh7003@gmail.com>
2025-03-23 21:33:45 +0530

    Store Component Fixed

--- src/components/Products/Store.jsx
+++ src/components/Products/Store.jsx
@@ -1,123 +1,89 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import ProductDetails from './ProductDetails'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const products = [
+    { id: 1, name: "Bioderma", price: "$55.00", oldPrice: "$95.00", img: "images/product_01.png", sale: true },
+    { id: 2, name: "Chanca Piedra", price: "$70.00", img: "images/product_02.png", sale: false },
+    { id: 3, name: "Umcka Cold Care", price: "$120.00", img: "images/product_03.png", sale: false },
+    { id: 4, name: "Cetyl Pure", price: "$20.00", oldPrice: "$45.00", img: "images/product_04.png", sale: true },
+    { id: 5, name: "CLA Core", price: "$38.00", img: "images/product_05.png", sale: false },
+    { id: 6, name: "Poo Pourri", price: "$38.00", oldPrice: "$89.00", img: "images/product_06.png", sale: true }
+];
+
 const Store = () => {
     return (
         <>
+            {/* Breadcrumb */}
             <div className="bg-light py-3">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-12 mb-0"><a href="">Home</a> <span className="mx-2 mb-0">/</span> <strong className="text-black">Store</strong></div>
+                        <div className="col-md-12 mb-0">
+                            <Link to="/">Home</Link> <span className="mx-2 mb-0">/</span> 
+                            <strong className="text-black">Store</strong>
+                        </div>
                     </div>
                 </div>
             </div>
 
+            {/* Store Section */}
             <div className="site-section">
                 <div className="container">
-
                     <div className="row">
+                        {/* Filter by Price */}
                         <div className="col-lg-6">
                             <h3 className="mb-3 h6 text-uppercase text-black d-block">Filter by Price</h3>
                             <div id="slider-range" className="border-primary"></div>
-                            <input type="text" name="text" id="amount" className="form-control border-0 pl-0 bg-white" disabled="" />
+                            <input type="text" id="amount" className="form-control border-0 pl-0 bg-white" disabled />
                         </div>
+
+                        {/* Filter by Reference */}
                         <div className="col-lg-6">
                             <h3 className="mb-3 h6 text-uppercase text-black d-block">Filter by Reference</h3>
-                            <button type="button" className="btn btn-secondary btn-md dropdown-toggle px-4" id="dropdownMenuReference"
-                                data-toggle="dropdown">Reference</button>
+                            <button type="button" className="btn btn-secondary btn-md dropdown-toggle px-4" 
+                                id="dropdownMenuReference" data-toggle="dropdown">
+                                Reference
+                            </button>
                             <div className="dropdown-menu" aria-labelledby="dropdownMenuReference">
-                                <a className="dropdown-item" href="#">Relevance</a>
-                                <a className="dropdown-item" href="#">Name, A to Z</a>
-                                <a className="dropdown-item" href="#">Name, Z to A</a>
+                                <Link className="dropdown-item" to="#">Relevance</Link>
+                                <Link className="dropdown-item" to="#">Name, A to Z</Link>
+                                <Link className="dropdown-item" to="#">Name, Z to A</Link>
                                 <div className="dropdown-divider"></div>
-                                <a className="dropdown-item" href="#">Price, low to high</a>
-                                <a className="dropdown-item" href="#">Price, high to low</a>
+                                <Link className="dropdown-item" to="#">Price, low to high</Link>
+                                <Link className="dropdown-item" to="#">Price, high to low</Link>
                             </div>
                         </div>
                     </div>
 
+                    {/* Product List */}
                     <div className="row">
-                        <div className="col-sm-6 col-lg-4 text-center item mb-4">
-                            <span className="tag">Sale</span>
-                            <Link to={<ProductDetails />}> <img src="images/product_01.png" alt="Image" /></Link>
-                            <h3 className="text-dark"><Link to={<ProductDetails />}>Bioderma</Link></h3>
-                            <p className="price"><del>95.00</del> &mdash; $55.00</p>
-                        </div>
-                        <div className="col-sm-6 col-lg-4 text-center item mb-4">
-                            <Link to={<ProductDetails />}> <img src="images/product_02.png" alt="Image" /></Link>
-                            <h3 className="text-dark"><Link to={<ProductDetails />}>Chanca Piedra</Link></h3>
-                            <p className="price">$70.00</p>
-                        </div>
-                        <div className="col-sm-6 col-lg-4 text-center item mb-4">
-                            <Link to={<ProductDetails />}> <img src="images/product_03.png" alt="Image" /></Link>
-                            <h3 className="text-dark"><Link to={<ProductDetails />}>Umcka Cold Care</Link></h3>
-                            <p className="price">$120.00</p>
-                        </div>
-
-                        <div className="col-sm-6 col-lg-4 text-center item mb-4">
-
-                            <Link to={<ProductDetails />}> <img src="images/product_04.png" alt="Image" /></Link>
-                            <h3 className="text-dark"><Link to={<ProductDetails />}>Cetyl Pure</Link></h3>
-                            <p className="price"><del>45.00</del> &mdash; $20.00</p>
-                        </div>
-                        <div className="col-sm-6 col-lg-4 text-center item mb-4">
-                            <Link to={<ProductDetails />}> <img src="images/product_05.png" alt="Image" /></Link>
-                            <h3 className="text-dark"><Link to={<ProductDetails />}>CLA Core</Link></h3>
-                            <p className="price">$38.00</p>
-                        </div>
-                        <div className="col-sm-6 col-lg-4 text-center item mb-4">
-                            <span className="tag">Sale</span>
-                            <Link to={<ProductDetails />}> <img src="images/product_06.png" alt="Image" /></Link>
-                            <h3 className="text-dark"><Link to={<ProductDetails />}>Poo Pourri</Link></h3>
-                            <p className="price"><del>$89</del> &mdash; $38.00</p>
-                        </div>
-
-                        <div className="col-sm-6 col-lg-4 text-center item mb-4">
-                            <span className="tag">Sale</span>
-                            <Link to={<ProductDetails />}> <img src="images/product_01.png" alt="Image" /></Link>
-                            <h3 className="text-dark"><Link to={<ProductDetails />}>Bioderma</Link></h3>
-                            <p className="price"><del>95.00</del> &mdash; $55.00</p>
-                        </div>
-                        <div className="col-sm-6 col-lg-4 text-center item mb-4">
-                            <Link to={<ProductDetails />}> <img src="images/product_02.png" alt="Image" /></Link>
-                            <h3 className="text-dark"><Link to={<ProductDetails />}>Chanca Piedra</Link></h3>
-                            <p className="price">$70.00</p>
-                        </div>
-                        <div className="col-sm-6 col-lg-4 text-center item mb-4">
-                            <Link to={<ProductDetails />}> <img ˀsrc="images/product_03.png" alt="Image" /></Link>
-                            <h3 className="text-dark"><Link to={<ProductDetails />}>Umcka Cold Care</Link></h3>
-                            <p className="price">$120.00</p>
-                        </div>
-
-                        <div className="col-sm-6 col-lg-4 text-center item mb-4">
-
-                            <Link to={<ProductDetails />}> <img src="images/product_04.png" alt="Image" /></Link>
-                            <h3 className="text-dark"><Link to={<ProductDetails />}>Cetyl Pure</Link></h3>
-                            <p className="price"><del>45.00</del> &mdash; $20.00</p>
-                        </div>
-                        <div className="col-sm-6 col-lg-4 text-center item mb-4">
-                            <Link to={<ProductDetails />}> <img src="images/product_05.png" alt="Image" /></Link>
-                            <h3 className="text-dark"><Link to={<ProductDetails />}>CLA Core</Link></h3>
-                            <p className="price">$38.00</p>
-                        </div>
-                        <div className="col-sm-6 col-lg-4 text-center item mb-4">
-                            <span className="tag">Sale</span>
-                            <Link to={<ProductDetails />}> <img src="images/product_06.png" alt="Image" /></Link>
-                            <h3 className="text-dark"><Link to={<ProductDetails />}>Poo Pourri</Link></h3>
-                            <p className="price"><del>$89</del> &mdash; $38.00</p>
-                        </div>
+                        {products.map((product) => (
+                            <div key={product.id} className="col-sm-6 col-lg-4 text-center item mb-4">
+                                {product.sale && <span className="tag">Sale</span>}
+                                <Link to={`/product-details/${product.id}`}>
+                                    <img src={product.img} alt={product.name} />
+                                </Link>
+                                <h3 className="text-dark">
+                                    <Link to={`/product-details/${product.id}`}>{product.name}</Link>
+                                </h3>
+                                <p className="price">
+                                    {product.oldPrice && <del>{product.oldPrice}</del>} &mdash; {product.price}
+                                </p>
+                            </div>
+                        ))}
                     </div>
+
+                    {/* Pagination */}
                     <div className="row mt-5">
                         <div className="col-md-12 text-center">
                             <div className="site-block-27">
                                 <ul>
-                                    <li><a href="#">&lt;</a></li>
+                                    <li><Link to="#">&lt;</Link></li>
                                     <li className="active"><span>1</span></li>
-                                    <li><a href="#">2</a></li>
-                                    <li><a href="#">3</a></li>
-                                    <li><a href="#">4</a></li>
-                                    <li><a href="#">5</a></li>
-                                    <li><a href="#">&gt;</a></li>
+                                    <li><Link to="#">2</Link></li>
+                                    <li><Link to="#">3</Link></li>
+                                    <li><Link to="#">4</Link></li>
+                                    <li><Link to="#">5</Link></li>
+                                    <li><Link to="#">&gt;</Link></li>
                                 </ul>
                             </div>
                         </div>
@@ -125,7 +91,7 @@ const Store = () => {
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default Store
+export default Store;
